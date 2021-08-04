e47f5e195732ac4301fab326b9aebd2ce4e91a18
vickydarlinn <uttapalsangwan@gmail.com>
2021-08-04 20:42:22 +0530

    testing

--- app.js
+++ app.js
@@ -8,22 +8,3 @@ const quantityOfStocks = Number(
 const currentStocksPrice = Number(
   document.querySelector(".currentstocksprice").value
 );
-const output = document.querySelector(".output").textContent;
-
-console.log(output);
-let bg = document.querySelector(".output_data").style.backgroundColor;
-bg = "red";
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", function () {
-  if (
-    initialStocksPrice >= 0 ||
-    quantityOfStocks >= 0 ||
-    currentStocksPrice >= 0
-  ) {
-    output = "Please inter proper value😔";
-    bg = "red";
-  } else {
-    output = "Please inter proper value😔";
-    bg = "red";
-  }
-});
