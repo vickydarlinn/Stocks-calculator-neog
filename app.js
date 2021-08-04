c1fe584a8336542f0abcf753713452e89a2e934f
vickydarlinn <uttapalsangwan@gmail.com>
2021-08-04 21:19:22 +0530

    fix js

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ const quantityOfStocks = Number(
 const currentStocksPrice = Number(
   document.querySelector(".currentstocksprice").value
 );
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".output");
+btn.addEventListener("click", function () {
+  if (
+    initialStocksPrice <= 0 ||
+    quantityOfStocks <= 0 ||
+    currentStocksPrice <= 0
+  ) {
+    output.style.backgroundColor = "black";
+    output.style.textAlign = "center";
+    output.textContent = "Please put proper values 😔 ";
+  } else {
+    let buy = initialStocksPrice * quantityOfStocks;
+    let sell = currentStocksPrice * quantityOfStocks;
+    let pl = sell - buy;
+    if (pl === 0) {
+      output.textContent = "No profit no loss 😑 ";
+    } else if (pl > 0) {
+      output.textContent = `waow...You are in profit of ${
+        (pl / buy) * 100
+      }% which is ${pl}Rs. It's party🎉 time...paytm me ${pl / 4}Rs.`;
+      output.style.backgroundColor = "green";
+    } else {
+      output.textContent = `You are in loss of ${
+        -(pl / buy) * 100
+      }% which is ${-pl}Rs.😥`;
+      output.style.backgroundColor = "red";
+    }
+  }
+});
